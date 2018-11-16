44f82abb96e6ded30f878c257270cc9084279589
Edgar Albalate <dev.gardo@gmail.com>
2018-11-16 17:38:18 +0100

    Added empty data tests

--- main_test.go
+++ main_test.go
@@ -10,12 +10,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateBookWithNonExistingAuthor(t *testing.T) {
+func TestGetEmptyData(t *testing.T) {
 	if err := initDB(); err != nil {
 		panic("failed to connect database")
 	}
 
-	db.LogMode(true)
+	defer db.Close()
+
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/books", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "[]", w.Body.String())
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/books/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+
+	body, _ := json.Marshal(&Book{
+		Title: "New title",
+	})
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("PUT", "/api/books/1", bytes.NewBuffer(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/books/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+}
+
+func TestCreateBookWithNonExistingAuthor(t *testing.T) {
+	if err := initDB(); err != nil {
+		panic("failed to connect database")
+	}
 
 	defer db.Close()
 
@@ -38,13 +74,6 @@ func TestPingRoute(t *testing.T) {
 
 	router := setupRouter()
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/books", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "[]", w.Body.String())
-
 	body, _ := json.Marshal(&Book{
 		Title: "The first book",
 		Isbn:  "981234155123-12312",
@@ -53,7 +82,9 @@ func TestPingRoute(t *testing.T) {
 			Lastname:  "Weleer",
 		},
 	})
-	req, _ = http.NewRequest("POST", "/api/books", bytes.NewBuffer(body))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/books", bytes.NewBuffer(body))
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
